8c5d458ee4dc4b7643186da395839731dccc45b1
Jean-Philippe Lebel <jp@lebel.pro>
2025-08-19 19:11:59 -0400

    Remove code leftover from older UE version

--- Source/FirstPersonShootCPP/FirstPersonShootCPPCharacter.h
+++ Source/FirstPersonShootCPP/FirstPersonShootCPPCharacter.h
@@ -30,14 +30,6 @@ class AFirstPersonShootCPPCharacter : public ACharacter
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FirstPersonCameraComponent;
 
-	/** Motion controller (right hand) */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UMotionControllerComponent* R_MotionController;
-
-	/** Motion controller (left hand) */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UMotionControllerComponent* L_MotionController;
-
 public:
 	AFirstPersonShootCPPCharacter();
 	UcoreDSEngine *Engine;
